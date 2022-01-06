6fee940bd5359840e89a22c087c1150c2dd61991
deadlygreenz <73725516+deadlygreenz@users.noreply.github.com>
2022-01-06 09:54:47 -0500

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,15 @@ import firebase from 'firebase';
 
 // add SDK Firebase
 var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyDck-Bc3mpfxNpNdGC3AnvCKq7uKrUXthE",
+  authDomain: "project-58-a76a3.firebaseapp.com",
+  databaseURL: "https://project-58-a76a3-default-rtdb.firebaseio.com",
+  projectId: "project-58-a76a3",
+  storageBucket: "project-58-a76a3.appspot.com",
+  messagingSenderId: "292719705719",
+  appId: "1:292719705719:web:6bbb17a4fc287a97699072"
+};
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
